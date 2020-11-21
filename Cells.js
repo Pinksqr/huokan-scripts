902c161d90394846db47213de03d5f41de884e57
Pinksquare <eliah.aquilina@gmail.com>
2020-11-21 17:06:06 -0500

    Created Cells.js to store hardcoded cell locations.

---
+++ Cells.js
@@ -0,0 +1,79 @@
+const CELLS_RAIDINFO_ARMORTYPES = {
+    "PLATE"     : "M8",
+    "MAIL"      : "M9",
+    "LEATHER"   : "M10",
+    "CLOTH"     : "M11"
+};
+
+const CELLS_RAIDINFO_MAINSTATS = {
+    "STRENGTH"  : "P8",
+    "AGILITY"   : "P9",
+    "INTELLECT" : "P10"
+};
+
+const CELLS_RAIDINFO_CLASSES = {
+    "WARRIOR"       : "J8",
+    "PALADIN"       : "J9",
+    "HUNTER"        : "J10",
+    "ROGUE"         : "J11",
+    "PRIEST"        : "J12",
+    "DEATH KNIGHT"  : "J13",
+    "SHAMAN"        : "J14",
+    "MAGE"          : "J15",
+    "WARLOCK"       : "J16",
+    "MONK"          : "J17",
+    "DRUID"         : "J18",
+    "DEMON HUNTER"  : "J19"
+};
+
+const CELLS_RAIDINFO_WEAPONS = {
+    "ABOMINABLE"    : "S8",
+    "MYSTIC"        : "S9",
+    "VENERATED"     : "S10",
+    "ZENITH"        : "S11"
+};
+
+const CELLS_RAIDINFO_TRINKETS = {
+    "SKULKER'S WING"                : "V8",
+    "BARGAST'S LEASH"               : "V9",
+    "GLUTTINOUS SPIKE"              : "V10",
+    "CONSUMPTIVE INFUSION"          : "V11",
+    "GLYPH OF ASSIMILATION"         : "V12",
+    "SPLINTERED HEART OF AL'AR"     : "V13",
+    "TUFT OF SMOULDERING PLUMAGE"   : "V14",
+    "SOUL IGNITER"                  : "V15",
+    "MEMORY OF PAST SINS"           : "V16",
+    "MACABRE SHEET MUSIC"           : "V17",
+    "HATEFUL CHAIN"                 : "V18",
+    "STONE LEGION HERALDRY"         : "V19",
+    "SANGUINE VINTAGE"              : "V20",
+    "CABALIST'S HYMNAL"             : "V21",
+    "MANABOUND MIRROR"              : "V22",
+    "DREADFIRE VESSEL"              : "V23"
+};
+
+const CELLS_RESERVATIONS_ARMORTYPES = {
+    "PLATE"     : "C24",
+    "MAIL"      : "C25",
+    "LEATHER"   : "C26",
+    "CLOTH"     : "C27"
+};
+
+const CELLS_RESERVATIONS_MAINSTATS = {}
+
+const CELLS_RESERVATIONS_CLASSES = {};
+
+const COLUMNS_RAIDMAINS = {
+    AVAILABLE       : 1,
+    PLAYER_NAME     : 2,
+    PLAYER_CLASS    : 3,
+    LOOT_SPECS      : [4, 6, 8, 10]
+};
+
+const COLUMNS_RAIDALTS = {
+    AVAILABLE   : 1,
+    MAIN_NAME   : 2,
+    ALT_NAME    : 3,
+    ALT_CLASS   : 4,
+    LOOT_SPECS  : [5, 7, 9, 11]
+};
