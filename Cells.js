62f6bcea1960d347633b5c1ba9e2d8f0555ff947
Pinksquare <eliah.aquilina@gmail.com>
2020-11-22 00:25:54 -0500

    Added and updated more hard-coded A1 cell values and column numbers

--- Cells.js
+++ Cells.js
@@ -1,3 +1,37 @@
+/** RAID INFORMATION SHEET */
+
+const CELLS_RAIDINFO_INFO = {
+    "GUILD" : "C7",
+    "DATE" : "C8",
+    "TIME" : "C9"
+};
+
+const CELLS_RAIDINFO_CARRIES = {
+    "SHRIEKWING"            : "F8",
+    "HUNTSMAN ALTIMOR"      : "F9",
+    "SUN KING'S SALVATION"  : "F10",
+    "ARTIFICER XY'MOX"      : "F11",
+    "HUNGERING DESTROYER"   : "F12",
+    "LADY INERVA DARKVEIN"  : "F13",
+    "THE COUNCIL OF BLOOD"  : "F14",
+    "SLUDGEFIST"            : "F15",
+    "STONE LEGION GENERALS" : "F16",
+    "SIRE DENATHRIUS"       : "F17",
+};
+
+const CELLS_RAIDINFO_FUNNELS = {
+    "SHRIEKWING"            : "G8",
+    "HUNTSMAN ALTIMOR"      : "G9",
+    "SUN KING'S SALVATION"  : "G10",
+    "ARTIFICER XY'MOX"      : "G11",
+    "HUNGERING DESTROYER"   : "G12",
+    "LADY INERVA DARKVEIN"  : "G13",
+    "THE COUNCIL OF BLOOD"  : "G14",
+    "SLUDGEFIST"            : "G15",
+    "STONE LEGION GENERALS" : "G16",
+    "SIRE DENATHRIUS"       : "G17",
+};
+
 const CELLS_RAIDINFO_ARMORTYPES = {
     "PLATE"     : "M8",
     "MAIL"      : "M9",
@@ -52,16 +86,95 @@ const CELLS_RAIDINFO_TRINKETS = {
     "DREADFIRE VESSEL"              : "V23"
 };
 
-const CELLS_RESERVATIONS_ARMORTYPES = {
-    "PLATE"     : "C24",
-    "MAIL"      : "C25",
-    "LEATHER"   : "C26",
-    "CLOTH"     : "C27"
+/** RESERVATION TEMPLATE SHEET */
+
+const CELLS_RESERVATIONS_INFO = {
+    "GUILD" : "C7",
+    "DATE" : "C8",
+    "TIME" : "C9"
 };
 
-const CELLS_RESERVATIONS_MAINSTATS = {}
+const CELLS_RESERVATIONS_CARRIES = {
+    "SHRIEKWING"            : "C8",
+    "HUNTSMAN ALTIMOR"      : "C9",
+    "SUN KING'S SALVATION"  : "C10",
+    "ARTIFICER XY'MOX"      : "C11",
+    "HUNGERING DESTROYER"   : "C12",
+    "LADY INERVA DARKVEIN"  : "C13",
+    "THE COUNCIL OF BLOOD"  : "C14",
+    "SLUDGEFIST"            : "C15",
+    "STONE LEGION GENERALS" : "C16",
+    "SIRE DENATHRIUS"       : "C17",
+}
 
-const CELLS_RESERVATIONS_CLASSES = {};
+const CELLS_RESERVATIONS_FUNNELS = {
+    "SHRIEKWING"            : "E8",
+    "HUNTSMAN ALTIMOR"      : "E9",
+    "SUN KING'S SALVATION"  : "E10",
+    "ARTIFICER XY'MOX"      : "E11",
+    "HUNGERING DESTROYER"   : "E12",
+    "LADY INERVA DARKVEIN"  : "E13",
+    "THE COUNCIL OF BLOOD"  : "E14",
+    "SLUDGEFIST"            : "E15",
+    "STONE LEGION GENERALS" : "E16",
+    "SIRE DENATHRIUS"       : "E17",
+}
+
+const CELLS_RESERVATIONS_ARMORTYPES = {
+    "PLATE"     : "C20",
+    "MAIL"      : "C21",
+    "LEATHER"   : "C22",
+    "CLOTH"     : "C23"
+};
+
+const CELLS_RESERVATIONS_MAINSTATS = {
+    "STRENGTH"  : "C26",
+    "AGILITY"   : "C27",
+    "INTELLECT" : "C28"
+}
+
+const CELLS_RESERVATIONS_CLASSES = {
+    "WARRIOR"       : "C41",
+    "PALADIN"       : "C42",
+    "HUNTER"        : "C43",
+    "ROGUE"         : "C44",
+    "PRIEST"        : "C45",
+    "DEATH KNIGHT"  : "C46",
+    "SHAMAN"        : "C47",
+    "MAGE"          : "C48",
+    "WARLOCK"       : "C49",
+    "MONK"          : "C50",
+    "DRUID"         : "C51",
+    "DEMON HUNTER"  : "C52"
+};
+
+const CELLS_RESERVATIONS_WEAPONS = {
+    "ABOMINABLE"    : "C31",
+    "MYSTIC"        : "C32",
+    "VENERATED"     : "C33",
+    "ZENITH"        : "C34"
+};
+
+const CELLS_RESERVATIONS_TRINKETS = {
+    "SKULKER'S WING"                : "C37",
+    "BARGAST'S LEASH"               : "C38",
+    "GLUTTINOUS SPIKE"              : "C39",
+    "CONSUMPTIVE INFUSION"          : "C40",
+    "GLYPH OF ASSIMILATION"         : "C41",
+    "SPLINTERED HEART OF AL'AR"     : "C42",
+    "TUFT OF SMOULDERING PLUMAGE"   : "C43",
+    "SOUL IGNITER"                  : "C44",
+    "MEMORY OF PAST SINS"           : "C45",
+    "MACABRE SHEET MUSIC"           : "C46",
+    "HATEFUL CHAIN"                 : "C47",
+    "STONE LEGION HERALDRY"         : "C48",
+    "SANGUINE VINTAGE"              : "C49",
+    "CABALIST'S HYMNAL"             : "C50",
+    "MANABOUND MIRROR"              : "C51",
+    "DREADFIRE VESSEL"              : "C52"
+};
+
+/** RAID MAINS & RAID ALTS SHEET */
 
 const COLUMNS_RAIDMAINS = {
     AVAILABLE       : 1,
@@ -76,4 +189,16 @@ const COLUMNS_RAIDALTS = {
     ALT_NAME    : 3,
     ALT_CLASS   : 4,
     LOOT_SPECS  : [5, 7, 9, 11]
+};
+
+const COLUMNS_RESERVATIONS = {
+    BUYER_NAME      : 11,
+    CLASS           : 14,
+    SPEC            : 15,
+    SERVICE         : 17,
+    FUNNELS         : 18,
+    FUNNEL_TYPE     : 19,
+    FUNNEL_OPTION   : 20,
+    NOTE            : 21,
+    CHECKBOX        : 22
 };
