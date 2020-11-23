c8bbc72281c9f35d8fdf87bc90b2a7c88f86190e
Pinksquare <eliah.aquilina@gmail.com>
2020-11-23 03:57:16 -0500

    Finished checkReservation() function(s) to match buyers to boosters, and added helper functions from Classes.js

--- Code.js
+++ Code.js
@@ -97,14 +97,14 @@ function createRaidRoster() {
 
         let main = getMainByName(mainName);
 
-        if (main && main.isAvailable() && available && altName && altClass && lootSpecs) {
+        if (main && main.available && available && altName && altClass && lootSpecs) {
             main.addAlt(altName, altClass.toUpperCase(), lootSpecs, available);
         }
     }
 
     function getMainByName(name) {
         for (let index in raidRoster) {
-            if (raidRoster[index].getPlayerName() === name) {
+            if (raidRoster[index].playerName() === name) {
                 return raidRoster[index];
             }
         }
@@ -132,7 +132,6 @@ function updateProperties(raidRoster) {
     updateValues(maxValues.maxClassNames, CELLS_RAIDINFO_CLASSES);
 
     //Add raidRoster to properties (must be present in case a new reservation sheet is made)
-    Logger.log(JSON.stringify(raidRoster));
     properties.setProperty("raidRoster", JSON.stringify(raidRoster));
 
     function clearValues(rangeArray) {
@@ -200,50 +199,213 @@ function checkReservation(sheet, range, value) {
         let row = range.getRow();
         let col = range.getColumn();
 
-        if (row > 7 && col === COLUMNS_RESERVATIONS.CHECKBOX && value) {
-            let carryCheck = isCarryAvailable(sheet.getRange(row, COLUMNS_RESERVATIONS.SERVICE).getValue());
-            let funnelCheck = isFunnelAvailable(sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNELS).getValue());
+        if (row > 7 && col === COLUMNS_RESERVATIONS.CHECKBOX && range.isChecked()) {
+            let raidRoster  = JSON.parse(sheet.getRange("A1").getValue());
+            let buyer = {
+                buyerName   : sheet.getRange(row, COLUMNS_RESERVATIONS.BUYER_NAME).getValue(),
+                className   : sheet.getRange(row, COLUMNS_RESERVATIONS.CLASS).getValue().toUpperCase(),
+                service     : sheet.getRange(row, COLUMNS_RESERVATIONS.SERVICE).getValue().toUpperCase(),
+                funnels     : sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNELS).getValue(),
+                funnelType  : sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNEL_TYPE).getValue().toUpperCase(),
+                funnelOpt   : sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNEL_OPTION).getValue().toUpperCase(),
+            }
 
-            if (carryCheck && funnelCheck) {
-                let raidRoster  = JSON.parse(sheet.getRange("A1").getValue());
-                let buyerName   = sheet.getRange(row, COLUMNS_RESERVATIONS.BUYER_NAME).getValue();
-                let service     = sheet.getRange(row, COLUMNS_RESERVATIONS.SERVICE).getValue();
-                let funnels     = sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNELS).getValue();
-                let funnelType  = sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNEL_TYPE).getValue();
-                let funnelOpt   = sheet.getRange(row, COLUMNS_RESERVATIONS.FUNNEL_OPTION).getValue();
-
-                Logger.log(buyerName + service + funnels + funnelType + funnelOpt);
-
-                //TODO: Find booster for buyer with raidRoster
-                for (let funnelNum = 0; funnelNum <= funnels; funnelNum++) {
-                    for (let booster in raidRoster) {
-                        Logger.log(raidRoster[booster]);
-                    }
+            /** Checks if a carry is available, then determines if regular service or a funnel service */
+            if (isCarryAvailable(sheet, buyer)) {
+                switch (true) {
+                    case buyer.funnels > 0:
+                        reserveFunnel(sheet, raidRoster, buyer);
+                        break;
+                    case buyer.funnels === 0:
+                        reserveCarry(buyer);
+                        break;
                 }
-
-                //TODO: Change booster(s) buyerName to buyer
-                //TODO: Recalculate current values
-                //let currentValues = getMaxValues(raidRoster);
+            } else {
+                Logger.log("Not enough carry spots are available!")
             }
         }
     }
 
     /** Checks if a carry spot is available */
-    function isCarryAvailable(service) {
-        return true;
+    function isCarryAvailable(sheet, buyer) {
+        for (let bossName in CELLS_RESERVATIONS_CARRIES) {
+            if (buyer.service === bossName) {
+                let carriesAvail = sheet.getRange(CELLS_RESERVATIONS_CARRIES[bossName].AVAIL).getValue();
+                return carriesAvail > 0;
+            }
+        }
     }
 
     /** Checks if a funnel spot is available */
-    function isFunnelAvailable(funnelNumber) {
-        return true;
+    function isFunnelAvailable(sheet, buyer) {
+        for (let bossName in CELLS_RESERVATIONS_FUNNELS) {
+            if (buyer.service === bossName) {
+                let funnelsAvail = sheet.getRange(CELLS_RESERVATIONS_FUNNELS[bossName].AVAIL).getValue();
+                return funnelsAvail >= buyer.funnels;
+            }
+        }
     }
 
-}
+    /** Reserves a carry by decreasing the number of available carries (for the service selected) */
+    function reserveCarry(buyer) {
+        let carriesAvail = sheet.getRange(CELLS_RESERVATIONS_CARRIES[buyer.service].AVAIL).getValue();
+        sheet.getRange(CELLS_RESERVATIONS_CARRIES[buyer.service].AVAIL).clearContent().setValue(carriesAvail - 1);
+    }
 
-function processReservation(sheet, range, value) {
-    let raidRoster = JSON.parse(sheet.getRange("A1").getValue());
+    function reserveFunnel(sheet, raidRoster, buyer) {
+        if (isFunnelAvailable(sheet, buyer)) {
+            let matches = [];
 
+            //For each funnel
+            for (let num = 0; num < buyer.funnels; num++) {
+                let funnelFound = false;
 
+                //For each raid member (while funnel not found)
+                for (let index = 0; (index < raidRoster.length && !funnelFound); index++){
+                    let player = raidRoster[index];
+                    let alts = player.alts
+
+                    //If player is available, does not have a buyer (or alts with a buyer), and has not already been matched (or has alts matched)
+                    if (player.available && !hasBuyer(player) && !isMatched(matches, index)) {
+                        if (isMatch(player, buyer)) {
+                            Logger.log(player.playerName)
+                            funnelFound = true;
+                            matches.push({mainIndex : index, altIndex : -1})
+                        } else {
+                            for (let altIndex = 0; (altIndex < alts.length && !funnelFound); altIndex++) {
+                                let alt = alts[altIndex];
+                                if (alt.available) {
+                                    if (isMatch(alt, buyer)) {
+                                        Logger.log(alt.playerName + " " + index + " " + altIndex)
+                                        funnelFound = true;
+                                        matches.push({mainIndex: index, altIndex: altIndex});
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            if (matches.length === buyer.funnels) {
+                Logger.log("Found funnels! Pairing up boosters with " + buyer.buyerName + " for " + buyer.funnels + " " + buyer.funnelType)
+                updateData(sheet, raidRoster, matches, buyer);
+            } else {
+                Logger.log("Funnel spots are available, but not enough boosters meet the criteria!")
+            }
+
+        } else {
+            Logger.log("Not enough funnel spots available!")
+        }
+
+        function isMatched(matches, index) {
+            let isMatched = false;
+            matches.forEach(function (match) {
+                if (match.mainIndex === index) {
+                    isMatched = true;
+                }
+            });
+            return isMatched;
+        }
+
+        function updateData(sheet, raidRoster, matches, buyer) {
+            //Update buyerName in raidRoster data
+            matches.forEach(function (match) {
+                if (match.altIndex >= 0) {
+                    raidRoster[match.mainIndex].alts[match.altIndex].buyerName = buyer.buyerName;
+                } else {
+                    raidRoster[match.mainIndex].buyerName = buyer.buyerName;
+                }
+            })
+
+            //Update max values (and raidRoster cell...)
+            updateAvailableValues(sheet, raidRoster, buyer);
+
+        }
+    }
+
+    function updateAvailableValues(sheet, raidRoster, buyer) {
+        let availableValues = getMaxValues(raidRoster);
+        Logger.log(raidRoster)
+        Logger.log(availableValues.maxArmorTypes)
+
+        //Update carry & funnel cells
+        let carryCell = CELLS_RESERVATIONS_CARRIES[buyer.service].AVAIL;
+        let funnelCell = CELLS_RESERVATIONS_FUNNELS[buyer.service].AVAIL;
+        let carryValue = sheet.getRange(carryCell).getValue();
+        let funnelValue = sheet.getRange(funnelCell).getValue();
+        sheet.getRange(carryCell).clearContent().setValue(carryValue - 1);
+        sheet.getRange(funnelCell).clearContent().setValue(funnelValue - buyer.funnels);
+
+        //Clear old values
+        clearValues(CELLS_RESERVATIONS_ARMORTYPES);
+        clearValues(CELLS_RESERVATIONS_MAINSTATS);
+        clearValues(CELLS_RESERVATIONS_WEAPONS);
+        clearValues(CELLS_RESERVATIONS_TRINKETS);
+
+        //Update values
+        updateValues(availableValues.maxArmorTypes, CELLS_RESERVATIONS_ARMORTYPES);
+        updateValues(availableValues.maxMainStats, CELLS_RESERVATIONS_MAINSTATS);
+        updateValues(availableValues.maxWeaponTokens, CELLS_RESERVATIONS_WEAPONS);
+        updateValues(availableValues.maxTrinkets, CELLS_RESERVATIONS_TRINKETS);
+
+        if (raidRoster) {
+            sheet.getRange("A1").clearContent().setValue(JSON.stringify(raidRoster));
+        }
+
+        function clearValues(rangeArray) {
+            for (const cell in rangeArray) {
+                sheet.getRange(rangeArray[cell].AVAIL).clearContent();
+            }
+        }
+
+        function updateValues(values, rangeArray) {
+            for (const value in values) {
+                sheet.getRange(rangeArray[value].AVAIL).setValue(values[value]);
+            }
+        }
+    }
+
+    /** Matches a player to a buyer */
+    function isMatch (player, buyer) {
+        switch (buyer.funnelType) {
+            case FUNNEL_TYPES.ARMOR:
+                if (CLASS_ARMORTYPES[player.playerClass] === CLASS_ARMORTYPES[buyer.className]) {
+                    return true;
+                }
+                break;
+
+            case FUNNEL_TYPES.CLASS:
+                if (player.playerClass === buyer.className) {
+                    return true;
+                }
+                break;
+
+            case FUNNEL_TYPES.WEAPON:
+                if (CLASS_TOKENS[player.playerClass] === CLASS_TOKENS[buyer.className]) {
+                    return true;
+                }
+                break;
+
+            case FUNNEL_TYPES.TRINKET:
+                let trinket = buyer.funnelOpt;
+                for (let index in player.lootSpecs) {
+                    if (player.lootSpecs[index]) {
+                        if (TRINKET_SPECS[trinket][CLASSES[player.playerClass]][index] === true){
+                            return true;
+                        }
+                    }
+                }
+                break;
+        }
+        return false;
+    }
+
+    /** Returns true if a player can loot a trinket for the spec specified */
+    function canLootTrinket(player, index, trinket) {
+        Logger.log(player.className + " " + TRINKET_SPECS[trinket][CLASSES[player.playerClass]][index])
+        return TRINKET_SPECS[trinket][CLASSES[player.playerClass]][index];
+    }
 }
 
 function createReservationSheet() {
@@ -261,6 +423,7 @@ function createReservationSheet() {
     }
 
     let raidRoster = properties.getProperty("raidRoster");
+    Logger.log(raidRoster);
 
     if (raidRoster) {
         newSheet.getRange("A1").clearContent().setValue(raidRoster);
@@ -277,7 +440,7 @@ function createReservationSheet() {
     function copyCells(sourceSheet, sourceCells, destSheet, destCells) {
         for (let cell in sourceCells) {
             let value = sourceSheet.getRange(sourceCells[cell]).getValue();
-            destSheet.getRange(destCells[cell]).setValue(value);
+            destSheet.getRange(destCells[cell].MAX).setValue(value);
         }
     }
 }
@@ -307,15 +470,16 @@ function getMaxValues(raidMembers) {
         }
 
         //TODO: Condense some duplicated code here
+        let playerHasBuyer = hasBuyer(main);
 
-        if (main.isAvailable() && !main.hasBuyer()) {
-            let lootSpecs = main.getLootSpecs();
-            let armorType = main.getArmorType();
-            let className = main.getClassName();
-            let weaponToken = main.getWeaponToken();
-            let trinkets = main.getTrinkets();
+        if (main.available && !playerHasBuyer) {
+            let lootSpecs = main.lootSpecs;
+            let armorType = CLASS_ARMORTYPES[main.playerClass];
+            let className = main.playerClass;
+            let weaponToken = CLASS_TOKENS[main.playerClass];
+            let trinkets = getTrinkets(main);
 
-            if (main.isSpecAvailable()) {
+            if (isSpecAvailable(main)) {
                 //Add values to maxValues object
                 maxValues.maxArmorTypes[armorType] = maxValues.maxArmorTypes[armorType] + 1 || 1;
                 maxValues.maxClassNames[className] = maxValues.maxClassNames[className] + 1 || 1;
@@ -324,7 +488,7 @@ function getMaxValues(raidMembers) {
                 //For each ACTIVE spec, add each unique main stat the specs provide (ex, Paladin provides Int & Str for all specs)
                 for (let index in lootSpecs) {
                     if (lootSpecs[index]) {
-                        let mainStat = main.getMainStat(index);
+                        let mainStat = SPEC_STATS[CLASSES[main.playerClass]][index];
                         if (mainStat && !addedValues.mainStats[mainStat]) {
                             maxValues.maxMainStats[mainStat] = maxValues.maxMainStats[mainStat] + 1 || 1;
                             addedValues.mainStats[mainStat] = true;
@@ -348,19 +512,17 @@ function getMaxValues(raidMembers) {
             }
         }
 
-        let alts = main.getAlts();
+        let alts = main.alts;
 
         alts.forEach(function(alt) {
+            if (alt.available){
+                let altLootSpecs = alt.lootSpecs;
+                let altArmorType = CLASS_ARMORTYPES[alt.playerClass];
+                let altClassName = alt.playerClass;
+                let altWeaponToken = CLASS_TOKENS[alt.playerClass];
+                let altTrinkets = getTrinkets(alt);
 
-            if (alt.isAvailable()){
-                let altLootSpecs = alt.getLootSpecs();
-                let altArmorType = alt.getArmorType();
-                let altClassName = alt.getClassName();
-                let altMainStats = alt.getMainStats();
-                let altWeaponToken = alt.getWeaponToken();
-                let altTrinkets = alt.getTrinkets();
-
-                if (main.isAvailable() && alt.isSpecAvailable()) {
+                if (main.available && isSpecAvailable(alt) && !playerHasBuyer) {
                     if (!addedValues.armorTypes[altArmorType]) {
                         maxValues.maxArmorTypes[altArmorType] = maxValues.maxArmorTypes[altArmorType] + 1 || 1;
                         addedValues.armorTypes[altArmorType] = true;
@@ -378,7 +540,7 @@ function getMaxValues(raidMembers) {
 
                     for (let index in altLootSpecs) {
                         if (altLootSpecs[index]) {
-                            let mainStat = alt.getMainStat(index);
+                            let mainStat = SPEC_STATS[CLASSES[alt.playerClass]][index];
                             if (mainStat && !addedValues.mainStats[mainStat]) {
                                 maxValues.maxMainStats[mainStat] = maxValues.maxMainStats[mainStat] + 1 || 1;
                                 addedValues.mainStats[mainStat] = true;
@@ -400,6 +562,46 @@ function getMaxValues(raidMembers) {
     })
 
     return maxValues;
+}
+
+/** Returns true if the player or any of their alts have a buyer assigned to them */
+function hasBuyer(player) {
+    let hasBuyer = false;
+    if (!player.buyerName){
+        player.alts.forEach(function(alt) {
+            if (alt.buyerName) {
+                hasBuyer = true;
+            }
+        })
+    }
+    return hasBuyer;
+}
+
+function isSpecAvailable(player) {
+    let specAvailable = false;
+    for (let spec in player.lootSpecs) {
+        if (player.lootSpecs[spec]) {
+            specAvailable = true;
+        }
+    }
+    return specAvailable;
+}
+
+function getTrinkets(player) {
+    let trinkets = [];
+
+    for (let index in player.lootSpecs) {
+        if (player.lootSpecs[index]) {
+            let specTrinkets = SPEC_TRINKETS[CLASSES[player.playerClass]][index];
+
+            if (index && specTrinkets) {
+                specTrinkets.forEach(function (trinket) {
+                    trinkets.push(trinket);
+                })
+            }
+        }
+    }
+    return trinkets;
 }
 
 function toTitleCase(str){
